df4efd200cebde26a281b95db4cc9bb8f9a65bef
Thomas Abela <sethabela91@gmail.com>
2016-05-22 18:47:51 +0200

    Menus
    Created A Title Menu with its own terrain and UI.
    Created an Instructions Menu with text explaining game objectives and
    controls.
    Added a Quit option to the Title Menu.
    
    Created a ButtonManager script to control loading different scenes when
    buttons are pressed.
    
    Adjusted Canvas Scaler so that all UI is scaled based on screen size.
    
    Organized Scenes in Build settings.

---
+++ Computer Interface Design Principles/Game Project/Unity3DPlatformer/Assets/scripts/MenuButtonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour {
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+	public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void InstButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void NextInst()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
